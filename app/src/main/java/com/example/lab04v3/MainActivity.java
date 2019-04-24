626f31d4fe7846c55d2b4cbf642265f649b130a7
lr39341 <lr39341@zut.edu.pl>
2019-04-24 16:52:44 +0200

    Dodano listę do apki

--- app/src/main/java/com/example/lab04v3/MainActivity.java
+++ app/src/main/java/com/example/lab04v3/MainActivity.java
@@ -2,8 +2,17 @@ package com.example.lab04v3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
