5af030d92a22429d9545e5d214c8448b4ed3231a
lr39341 <lr39341@zut.edu.pl>
2019-04-24 17:00:34 +0200

    Dodanie nowego formularza

---
+++ app/src/main/java/com/example/lab04v3/DodajWpis.java
@@ -0,0 +1,14 @@
+package com.example.lab04v3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DodajWpis extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_wpis);
+    }
+}
--- app/src/main/java/com/example/lab04v3/MainActivity.java
+++ app/src/main/java/com/example/lab04v3/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lab04v3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,4 +25,21 @@ public class MainActivity extends AppCompatActivity {
         //TODO
         //Komentarz do zrobienia
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+
 }
