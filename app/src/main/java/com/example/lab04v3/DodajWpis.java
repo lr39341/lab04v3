4f43b24f077329325c65a0545fd0f5663d879810
lr39341 <lr39341@zut.edu.pl>
2019-05-15 18:34:12 +0200

    Edycja wpisów

--- app/src/main/java/com/example/lab04v3/DodajWpis.java
+++ app/src/main/java/com/example/lab04v3/DodajWpis.java
@@ -10,6 +10,7 @@ import android.widget.Spinner;
 
 public class DodajWpis extends AppCompatActivity {
 
+    private int modyfi_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,23 @@ public class DodajWpis extends AppCompatActivity {
                 new String[] {"Pies", "Kot", "Rybki"});
         Spinner gatunek = (Spinner)findViewById(R.id.gatunek);
         gatunek.setAdapter(gatunki);
+
+        Bundle extras = getIntent().getExtras();
+        try {
+            if (extras.getSerializable("element") != null) {
+                Animal zwierz = (Animal) extras.getSerializable("element");
+                EditText kolor = (EditText) findViewById(R.id.kolor);
+                EditText wielkosc = (EditText) findViewById(R.id.wielkosc);
+                EditText opis = (EditText) findViewById(R.id.opis);
+
+                kolor.setText(zwierz.getKolor());
+                wielkosc.setText(Float.toString(zwierz.getWielkosc()));
+                opis.setText(zwierz.getOpis());
+                this.modyfi_id=zwierz.getID();
+            }
+        }catch(Exception ex) {
+            this.modyfi_id=0;
+        }
     }
 
     public void wyslij (View view) {
@@ -34,6 +52,8 @@ public class DodajWpis extends AppCompatActivity {
                 Float.valueOf(wielkosc.getText().toString()),
                 opis.getText().toString()
         );
+
+        zwierze.setId(this.modyfi_id);
 
         Intent intencja = new Intent();
         intencja.putExtra("nowy", zwierze);
--- app/src/main/java/com/example/lab04v3/MainActivity.java
+++ app/src/main/java/com/example/lab04v3/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
             Animal nowy = (Animal) extras.getSerializable("nowy");
 
             this.db.dodaj(nowy);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
+        }else if (requestCode == 2 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Animal nowy = (Animal) extras.getSerializable("nowy");
+
+            this.db.aktualizuj(nowy);
             adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
         }
